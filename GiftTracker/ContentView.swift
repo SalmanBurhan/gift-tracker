dfe6ddbbe6c250e0cff59838bc24d242b4f8f8d6
Salman Burhan <salmanfburhan@gmail.com>
2024-02-21 19:16:41 -0800

    Initial Commit

---
+++ GiftTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GiftTracker
+//
+//  Created by Salman Burhan on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GiftTracker/GiftTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftTrackerApp.swift
+//  GiftTracker
+//
+//  Created by Salman Burhan on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GiftTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
